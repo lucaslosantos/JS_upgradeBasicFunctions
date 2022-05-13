91a1e62df2054fe628578e15a8db39dc6eb3b6f6
lucaslosantos <lucaslosantosoliva@gmail.com>
2022-05-13 14:23:12 +0200

    Iteration 1

---
+++ functions.js
@@ -0,0 +1,11 @@
+function sum(numberOne , numberTwo) {
+    if (numberOne>numberTwo){
+        console.log (numberOne);
+    }
+    else if (numberTwo>numberOne){
+        console.log(numberTwo);
+    }
+    else console.log("Son iguales...")
+  }
+
+  sum(2,2);
